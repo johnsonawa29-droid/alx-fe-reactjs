047fe19f5722b9143601313dcedaa2def62f0ff7
johnsonawa29-driod <johnsonawa29@gmail.com>
2026-01-09 04:07:34 +0100

    yes

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,33 +1,15 @@
-import WelcomeMessage from './components/WelcomeMessage';
-
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import WelcomeMessage from './components/WelcomeMessage';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 function App() {
-  return (
-    <>
-      <WelcomeMessage />
-
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <h1>Vite + React</h1>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    );
 }
 
-export default App
+export default App;
