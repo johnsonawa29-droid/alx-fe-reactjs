34cf49325f5fe3a4ec20253bf188ac628a345ee8
johnsonawa29-driod <johnsonawa29@gmail.com>
2026-01-14 20:00:32 +0100

    yes

--- alx-react-app-new/src/app.jsx
+++ alx-react-app-new/src/app.jsx
@@ -1,8 +1,9 @@
- import Counter from './components/Counter';
- import Header from './components/Header';
+
+import Header from './components/Header';
 import MainContent from './components/MainContent';
 import UserProfile from './components/UserProfile';
 import Footer from './components/Footer';
+import Counter from './components/Counter';
 
 function App() {
   return (
@@ -12,8 +13,9 @@ function App() {
       <UserProfile
         name="Johnson"
         age={25}
-        bio="A passionate React developer learning component styling."
+        bio="A passionate React developer learning hooks."
       />
+      <Counter />
       <Footer />
     </div>
   );
