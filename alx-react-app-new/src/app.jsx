7ccefea3f3387db4dd3f4a7de5a168a1037ee56e
johnsonawa29-driod <johnsonawa29@gmail.com>
2026-01-14 19:50:59 +0100

    yes

--- alx-react-app-new/src/app.jsx
+++ alx-react-app-new/src/app.jsx
@@ -1,4 +1,5 @@
-import Header from './components/Header';
+ import Counter from './components/Counter';
+ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import UserProfile from './components/UserProfile';
 import Footer from './components/Footer';
