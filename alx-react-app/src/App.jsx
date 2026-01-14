5024bc0993f05d611ffd817c5b1b53528abe4a90
johnsonawa29-driod <johnsonawa29@gmail.com>
2026-01-14 02:53:52 +0100

    yes

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,15 +1,14 @@
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
+import UserProfile from "./components/UserProfile";
 
 function App() {
-    return (
-        <div>
-            <Header />
-            <MainContent />
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <UserProfile
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography"
+      />
+    </div>
+  );
 }
 
-export default App;
