655b0728ab7aca48f3319993bf94f23d4b2b1861
johnsonawa29-driod <johnsonawa29@gmail.com>
2026-01-21 00:45:01 +0100

    yes

--- recipe-sharing-app/src/components/RecipeDetail.jsx
+++ recipe-sharing-app/src/components/RecipeDetail.jsx
@@ -1,22 +1,25 @@
-import { useParams, Link } from 'react-router-dom';
-import { useRecipeStore } from './recipeStore';
-import EditRecipeForm from './EditRecipeForm';
-import DeleteRecipeButton from './DeleteRecipeButton';
+import { useParams } from "react-router-dom";
+import useRecipeStore from "./recipeStore";
+import EditRecipeForm from "./EditRecipeForm";
+import DeleteRecipeButton from "./DeleteRecipeButton";
 
 const RecipeDetails = () => {
-  const { id } = useParams(); // get recipe ID from URL
-  const recipe = useRecipeStore(state =>
-    state.recipes.find(r => r.id === id)
+  const { id } = useParams();
+  const recipe = useRecipeStore((state) =>
+    state.recipes.find((r) => r.id === Number(id))
   );
 
-  if (!recipe) return <p>Recipe not found!</p>;
+  if (!recipe) {
+    return <p>Recipe not found.</p>;
+  }
 
   return (
     <div>
       <h1>{recipe.title}</h1>
       <p>{recipe.description}</p>
-      <Link to={`/edit/${id}`}>Edit Recipe</Link>
-      <DeleteRecipeButton recipeId={id} />
+
+      <EditRecipeForm recipe={recipe} />
+      <DeleteRecipeButton recipeId={recipe.id} />
     </div>
   );
 };
