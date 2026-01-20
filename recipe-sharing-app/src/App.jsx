1019e5fa9fe2aed0499a84bb9032a6db82bb77af
johnsonawa29-driod <johnsonawa29@gmail.com>
2026-01-20 14:17:39 +0100

    yes

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,11 +1,14 @@
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
-    <div>
+    <div style={{ maxWidth: '600px', margin: '0 auto', padding: '1rem' }}>
       <h1>Recipe Sharing App</h1>
+
       <AddRecipeForm />
+      <SearchBar />
       <RecipeList />
     </div>
   );
