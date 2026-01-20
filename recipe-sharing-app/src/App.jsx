fd47de0dc3c8cef3ab8c3e2bf28742cbc444899e
johnsonawa29-driod <johnsonawa29@gmail.com>
2026-01-20 14:27:21 +0100

    yes

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,16 +1,35 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
 import SearchBar from './components/SearchBar';
 
 function App() {
   return (
-    <div style={{ maxWidth: '600px', margin: '0 auto', padding: '1rem' }}>
-      <h1>Recipe Sharing App</h1>
+    <Router>
+      <div style={{ maxWidth: '600px', margin: '0 auto', padding: '1rem' }}>
+        <h1>Recipe Sharing App</h1>
 
-      <AddRecipeForm />
-      <SearchBar />
-      <RecipeList />
-    </div>
+        {/* Navigation */}
+        <nav style={{ marginBottom: '1rem' }}>
+          <Link to="/" style={{ marginRight: '1rem' }}>Home</Link>
+          <Link to="/add">Add Recipe</Link>
+        </nav>
+
+        {/* Routes */}
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <SearchBar />
+                <RecipeList />
+              </>
+            }
+          />
+          <Route path="/add" element={<AddRecipeForm />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
