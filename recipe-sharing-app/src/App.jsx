5e60d9f1daeac26fac6bebd11969339fbcb624d1
johnsonawa29-driod <johnsonawa29@gmail.com>
2026-01-20 15:28:06 +0100

    yes

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,18 +1,24 @@
 // src/App.jsx
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
-import RecipeDetail from './components/RecipeDetail'; // create this component
+import RecipeDetails from './components/RecipeDetails';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<RecipeList />} />
-        <Route path="/recipes/:id" element={<RecipeDetail />} />
+        <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
       </Routes>
     </Router>
   );
 }
+
+// Wrapper to get recipeId from URL params
+import { useParams } from 'react-router-dom';
+const RecipeDetailsWrapper = () => {
+  const { id } = useParams();
+  return <RecipeDetails recipeId={id} />;
+};
 
 export default App;
