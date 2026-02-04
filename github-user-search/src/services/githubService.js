be8eef76a56773e0422b650362c16ea7a86a9c78
johnsonawa29-driod <johnsonawa29@gmail.com>
2026-02-04 11:48:36 +0100

    yes

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -3,11 +3,13 @@ import axios from "axios";
 const BASE_URL = "https://api.github.com";
 
 export const fetchAdvancedUsers = async (query) => {
-  const response = await axios.get(`${BASE_URL}/search/users`, {
-    params: {
-      q: query,
-    },
-  });
-
-  return response.data.items;
+  try {
+    const response = await axios.get(`${BASE_URL}/search/users`, {
+      params: { q: query },
+    });
+    return response.data.items;
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    return [];
+  }
 };
