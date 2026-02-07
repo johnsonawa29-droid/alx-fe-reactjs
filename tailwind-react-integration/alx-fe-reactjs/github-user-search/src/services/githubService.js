142b2caaeb2b375bddd33e388d19e2506759f229
johnsonawa29-driod <johnsonawa29@gmail.com>
2026-02-07 19:13:37 +0100

    yes

--- tailwind-react-integration/alx-fe-reactjs/github-user-search/src/services/githubService.js
+++
@@ -1,6 +0,0 @@
-const headers = {};
-if (process.env.REACT_APP_GITHUB_TOKEN) {
-  headers.Authorization = `token ${process.env.REACT_APP_GITHUB_TOKEN}`;
-}
-
-const response = await axios.get(url, { headers });
