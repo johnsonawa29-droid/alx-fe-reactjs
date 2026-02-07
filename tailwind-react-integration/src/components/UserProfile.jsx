03776cde92237b54daab7cd86a1a83ca616bb90e
johnsonawa29-driod <johnsonawa29@gmail.com>
2026-02-07 21:30:23 +0100

    yes

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -6,7 +6,7 @@ function UserProfile() {
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-32 h-32 sm:w-36 sm:h-36 md:w-36 md:h-36 mx-auto"
+        className="rounded-full w-32 h-32 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"
       />
       <h1 className="text-lg sm:text-lg md:text-xl text-blue-800 my-3 sm:my-4 text-center font-semibold">
         John Doe
