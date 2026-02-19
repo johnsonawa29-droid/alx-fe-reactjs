2c82b7f2a68e577c2f395077b2351d99a7fbed36
johnsonawa29-driod <johnsonawa29@gmail.com>
2026-02-19 11:04:21 +0100

    yes

---
+++ form-handling-react/src/components/formikForm.js
@@ -0,0 +1,49 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const FormikForm = () => {
+  const initialValues = {
+    username: "",
+    email: "",
+    password: "",
+  };
+
+  const validationSchema = Yup.object({
+    username: Yup.string().required("Username is required"),
+    email: Yup.string().required("Email is required"),
+    password: Yup.string().required("Password is required"),
+  });
+
+  const handleSubmit = (values, { resetForm }) => {
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(values),
+    });
+
+    resetForm();
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      <Form>
+        <Field name="username" />
+        <ErrorMessage name="username" />
+
+        <Field name="email" />
+        <ErrorMessage name="email" />
+
+        <Field name="password" type="password" />
+        <ErrorMessage name="password" />
+
+        <button type="submit">Register</button>
+      </Form>
+    </Formik>
+  );
+};
+
+export default FormikForm;
